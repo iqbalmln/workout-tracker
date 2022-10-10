7549fac06ea7f3a15a53b7cc8a8303d67cc3eacd
iqbalmln <iqbalmaulana15784@gmail.com>
2022-10-10 14:14:30 +0700

    feat: create register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,27 +5,76 @@
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
     <!-- Register -->
-    ini register
+    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+      <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          v-model="email"
+        />
+      </div>
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          v-model="password"
+        />
+      </div>
+      <div class="flex flex-col mb-2">
+        <label for="confirmPassword" class="mb-1 text-sm text-at-light-green"
+          >Confirm Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          v-model="confirmPassword"
+        />
+      </div>
+      <button
+        type="submit"
+        class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+        text-white bg-at-light-green duration-200
+        border-solid border-2 border-transparent
+        hover:border-white hover:bg-white
+        hover:text-at-light-green"
+      >
+        Register
+      </button>
+      <router-link :to="{name: 'Login'}" class="text-center mt-4 text-sm">Already have an account?<span class="text-at-light-green"> Login</span></router-link>
+    </form>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 export default {
   name: "register",
   setup() {
     // Create data / vars
     const email = ref(null);
-    const password = ref(null)
-    const confirmPassword = ref(null)
-    const errorMsg = ref(null)
+    const password = ref(null);
+    const confirmPassword = ref(null);
+    const errorMsg = ref(null);
     // Register function
 
     return {
       email,
       password,
       confirmPassword,
-      errorMsg
+      errorMsg,
     };
   },
 };
