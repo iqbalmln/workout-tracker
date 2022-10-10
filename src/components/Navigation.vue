725113db089a1ccb54a97faec259c576c3627893
iqbalmln <iqbalmaulana15784@gmail.com>
2022-10-10 17:02:22 +0700

    chore: conditional render for user login || logout

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,24 +7,32 @@
       </div>
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link :to="{name: 'Home'}" class="cursor-pointer">Home</router-link>
-        <router-link :to="{name: ''}" class="cursor-pointer">Create</router-link>
-        <router-link :to="{name: 'Login'}" class="cursor-pointer">Login</router-link>
-        <li class="cursor-pointer">Logout</li>
+        <router-link v-if="user" :to="{name: ''}" class="cursor-pointer">Create</router-link>
+        <router-link v-if="!user" :to="{name: 'Login'}" class="cursor-pointer">Login</router-link>
+        <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script>
+import store from "../store/index"
+import { supabase } from "../supabase/init"
+import {useRouter} from "vue-router"
+import { computed } from "vue";
 export default {
   setup() {
     // Get user from store
-
+    const user = computed(() => store.state.user)
     // Setup ref to router
+    const router = useRouter()
 
     // Logout function
-
-    return {};
+    const logout = async () => {
+      await supabase.auth.signOut()
+      router.push({name: 'Home'})
+    }
+    return {logout, user};
   },
 };
 </script>
