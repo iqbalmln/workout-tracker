d73934d51b0d6844a8e44ca373805f434c90041c
iqbalmln <iqbalmaulana15784@gmail.com>
2022-10-10 14:37:29 +0700

    feat: chore: create login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,74 @@
 <template>
-  <div>
-    ini login
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <!-- Error handling -->
+    <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey">
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+    <!-- Login -->
+    <form class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+      <h1 class="text-3xl text-at-light-green mb-4">Login</h1>
+      <div class="flex flex-col mb-2">
+        <label for="email" class="mb-1 text-sm text-at-light-green"
+          >Email</label
+        >
+        <input
+          type="email"
+          id="email"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          v-model="email"
+        />
+      </div>
+      <div class="flex flex-col mb-2">
+        <label for="password" class="mb-1 text-sm text-at-light-green"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          v-model="password"
+        />
+      </div>
+      <button
+        type="submit"
+        class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+        text-white bg-at-light-green duration-200
+        border-solid border-2 border-transparent
+        hover:border-white hover:bg-white
+        hover:text-at-light-green"
+      >
+        Login
+      </button>
+      <router-link
+        :to="{name: 'Register'}"
+        class="text-center mt-4 text-sm"
+      >
+        Don't have an account?
+        <span class="text-at-light-green">Register</span>
+      </router-link>
+    </form>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-  name: "login",
+  name: "register",
   setup() {
     // Create data / vars
+    const email = ref(null);
+    const password = ref(null);
+    const errorMsg = ref(null);
 
-    // Login function
+    // Register function
 
-    return {};
+    return {
+      email,
+      password,
+      errorMsg,
+    };
   },
 };
 </script>
